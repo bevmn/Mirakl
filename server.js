092a091b553eaf3c11f66018c23dc318621db9a7
Bev Nelsen <bevm.nelsen@gmail.com>
2018-08-26 19:27:41 -0500

    db update2

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // Handlebars
+
 app.engine(
   "handlebars",
   exphbs({
@@ -26,7 +27,7 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
