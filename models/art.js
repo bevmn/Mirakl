5f80da980c08acf17e98d61faa3811dcdaede72b
Chery Vang <misscheryvang@yahoo.com>
2018-08-28 05:07:52 -0500

    website working.

--- models/art.js
+++ models/art.js
@@ -1,10 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
-var Art = sequelize.define("Art", {
+var art = sequelize.define("art", {
+    image: { type: DataTypes.STRING },
     item_name: { type: DataTypes.STRING },
     category_: { type: DataTypes.STRING },
     artist_: { type: DataTypes.STRING },
     price_: { type: DataTypes.DECIMAL },
     stock_quantity_: { type: DataTypes.INTEGER },
 });
-return Art;
+return art;
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,34 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Loads gallery page
+  app.get("/gallery", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/gallery.html"));
+  });
+
+  // Loads seller page
+  app.get("/seller", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/seller.html"));
+  });
+
+  // Loads buyer page
+  app.get("/gallery", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/buyer.html"));
+  });
+
+  // Loads cart page
+  app.get("/gallery", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/cart.html"));
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
+
